cbe29bfbe8761a7948b9bd79c46483bed53bf947
Ali Vahedi <116800340+Ali-Vahedifar@users.noreply.github.com>
2026-02-17 15:42:45 +0100

    Update conference year from 2025 to 2026

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -8,7 +8,7 @@ Author: Ali Vahedi
 Affiliation: DIGIT and Department of ECE, Aarhus University, Denmark
 Email: av@ece.au.dk
 
-IEEE INFOCOM 2025
+IEEE INFOCOM 2026
 
 This implementation includes:
 - Multi-head self-attention mechanism
