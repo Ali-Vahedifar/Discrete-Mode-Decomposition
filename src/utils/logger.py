6a8caccbc3727dfefea76842a800d79a6509c86e
Ali Vahedi <116800340+Ali-Vahedifar@users.noreply.github.com>
2026-02-17 15:37:14 +0100

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,7 +5,7 @@ Logging Utilities
 Author: Ali Vahedi
 Affiliation: DIGIT and Department of ECE, Aarhus University, Denmark
 Email: av@ece.au.dk
-IEEE INFOCOM 2025
+IEEE INFOCOM 2026
 """
 
 import logging
