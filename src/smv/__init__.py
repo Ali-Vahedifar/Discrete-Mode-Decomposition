7fd9ac34f8e0597eac707a6f7f5e21ea79866d84
Ali Vahedi <116800340+Ali-Vahedifar@users.noreply.github.com>
2026-02-17 15:39:50 +0100

    Update conference year from 2025 to 2026

--- src/smv/__init__.py
+++ src/smv/__init__.py
@@ -9,7 +9,7 @@ Author: Ali Vahedi
 Affiliation: DIGIT and Department of ECE, Aarhus University, Denmark
 Email: av@ece.au.dk
 
-IEEE INFOCOM 2025
+IEEE INFOCOM 2026
 
 Key Components:
 --------------
